cd169a37ca73efd86c962dd09b7b35a4efbc24ac
Matthew M <mbmoorin@gmail.com>
2020-06-14 21:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value});
-  }
+}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
- }
+}
 
 function deleteFromObjectByKey(object, key) {
   
