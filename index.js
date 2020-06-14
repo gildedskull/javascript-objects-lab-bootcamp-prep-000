c2323858e5114a94ae92984823da80ba967cc077
Matthew M <mbmoorin@gmail.com>
2020-06-14 21:40:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {  
   object[key] = value
   return object
 }
 
 function deleteFromObjectByKey(object, key) {
   var obj = Object.assign({}, object)
-  
+  delete obj[key]
+  return object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
